1629adf7caa4b0defdba7f6a6646b24d100c1ead
JACOB TAN ZI HANG <22001027@myrp.edu.sg>
2023-04-26 15:01:54 +0800

    Initial commit

---
+++ DemoMyLoginApp/app/src/main/java/sg/edu/rp/c346/id22001027/demomyloginapp/MainActivity.java
@@ -0,0 +1,13 @@
+package sg.edu.rp.c346.id22001027.demomyloginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
